ad22282b611e2b577c8dd5487350e1e79741d8aa
Juleles <jw.custodio@sarria.salesians.cat>
2023-10-16 16:14:29 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Jules William Custodio 23-24"),
+      ),
+    )
+  ));
+}
